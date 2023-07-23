84b8d7531cf0ee8175d121920a297aa045398391
edehgoodness123 <goodnessgift022@gmail.com>
2023-07-23 21:56:34 +0100

    add files

--- script.js
+++ script.js
@@ -83,8 +83,40 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("corrrect");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
 }
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "play Again";
+    nextButton.style.display = "block";
+}
+
+function  handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < quetsions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+       handleNextButton(); 
+    }else{
+        startQuiz()
+    }
+})
 startQuiz();
